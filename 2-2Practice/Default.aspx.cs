443ada9c252fcdc444745bd412770e859210fd8a
奕淳 王 <forever2635@yahoo.com>
2021-03-09 17:12:34 +0800

    阿

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,28 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            int N = 444;
+            bool a = false;
 
+            if (N % 4 == 0) {
+                a = true;
+
+                if (N % 100 == 0) {
+                    if (N % 400 == 0) {
+                        a = true;
+                    } 
+                    else{
+                        a = false;
+                    }
+                } 
+            }
+
+           if (a == true) { 
+            Response.Write("Y");
+            }
+            else{
+                Response.Write("N");
+            }
         }
     }
 }

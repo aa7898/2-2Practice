1a431a6df01041f129b9c5c6d395881acbda8928
aa7898 <107111228@mail.oit.edu.tw>
2021-04-23 22:51:59 +0800

    2-3練習

---
+++ 2-2Practice/ReverseInt.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _2_2Practice
+{
+    public partial class ReverseInt : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            
+            int N = 1234;
+
+            string Y;
+
+            char[] ArrayN = N.ToString().ToCharArray();
+            
+            Array.Reverse(ArrayN);
+            
+            Y = new string(ArrayN);
+           
+            Label1.Text = N + "----->" + Y;
+            
+
+        }
+    }
+}
